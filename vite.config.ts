6116d1f62ba40af51e7454906f363c5c4cd34304
Masayasu Fujiwara <masayasu.fujiwara@autodesk.com>
2025-12-27 07:22:30 +0900

    /docsにデプロイするように。

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/flash-phrase/',
+  build: {
+    outDir: 'docs', // 出力先を docs に変更
+  },
   plugins: [
     react({
       babel: {
